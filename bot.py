3234e8632f18709dc54485431a4908b4f77e4860
ragu1student <120290026+ragu1student@users.noreply.github.com>
2023-03-26 09:39:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+
 from pyrogram import Client, filters
 from truecallerpy import search_phonenumber
 import re
-id = " a2i0H--bsDYqhFMFcXSk-tVykJleH_U2Qfz8RwhTujiofGGodRbk2DKOP6_eRg71"
+id = "a2i0H--bsDYqhFMFcXSk-tVykJleH_U2Qfz8RwhTujiofGGodRbk2DKOP6_eRg71"
 API_ID = "7280833"
 API_HASH = "faa0e3b6d30779426d833dec02ab70d9"
 TOKEN = "1969982366:AAHoE4d2bnRem_Ng8pL694z_jHQAXMJB6LA"
@@ -17,8 +18,8 @@ app = Client("tagremover", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH)
 ))
 async def tag(client, message):
     a = search_phonenumber(str(message.text),"IN", id)
-    b =[]
-    if 'name' in a['data'][0]:
+    b =['Truecaller result\n\n']
+    if a is not None and 'data' in a and len(a['data']) > 0 and 'name' in a['data'][0]:
         b.append("Name: " +  a['data'][0]['name'])
     if 'gender' in a['data'][0]:
         b.append("Gender:"+ a['data'][0]['gender'])
